df64da5c1ea9a033e8916f206f90ec85139f0fe9
akyrey <stein.dario@gmail.com>
2022-12-03 14:52:27 +0100

    feat: day 03 part 2 complete

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,8 +1,11 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    let mut index = 0;
+    let mut group = Vec::<char>::new();
     let mut sum = 0;
     // File hosts must exist in current path before this produces output
     // if let Ok(lines) = read_lines("./src/bin/test_03.txt") {
@@ -10,13 +13,22 @@ fn main() {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                let chars = ip.chars().collect::<Vec<char>>();
-                let rucksacks: Vec<Vec<char>> = chars.chunks(chars.len() / 2).map(|c| c.into()).collect();
-                let common_char = find_common_char(&rucksacks[0], &rucksacks[1]);
-                if let Some(item_type) = common_char {
-                    sum = sum + get_item_priority(item_type);
+                let remainder = index % 3;
+                if remainder == 0 {
+                    group = ip.chars().collect::<Vec<char>>();
+                } else {
+                    group = group
+                        .into_iter()
+                        .filter(|value| ip.chars().collect::<Vec<char>>().contains(value))
+                        .collect::<HashSet<char>>()
+                        .into_iter()
+                        .collect::<Vec<char>>();
+                }
+                if group.len() == 1 {
+                    sum = sum + get_item_priority(&group[0]);
                 }
             }
+            index = index + 1;
         }
     }
 
@@ -32,9 +44,14 @@ fn get_item_priority(value: &char) -> u32 {
     return int_value - 96;
 }
 
-fn find_common_char<'a>(first_rucksack: &Vec<char>, second_rucksack: &'a Vec<char>) -> Option<&'a char> {
-    return second_rucksack.iter().find(|current| first_rucksack.contains(&current));
-}
+// fn find_common_char<'a>(
+//     first_rucksack: &Vec<char>,
+//     second_rucksack: &'a Vec<char>,
+// ) -> Option<&'a char> {
+//     return second_rucksack
+//         .iter()
+//         .find(|current| first_rucksack.contains(&current));
+// }
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
